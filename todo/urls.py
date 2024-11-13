7c3d711a4777885bb0e34d2fbfbc1e7830e2ae2f
josephbill <josephbill00@gmail.com>
2024-11-13 16:41:21 +0300

    added update and delete processes for todoapp

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('task/create/', views.task_create, name='task_create'),
+    path('task/delete/<int:pk>/', views.task_delete, name='task_delete'),
+    path('task/update/<int:pk>/', views.task_update,
+         name='task_update'),
 ]
 
 # after defining the app routes
--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,7 @@
 ## a method to list our tasks
 ## a method to create the tasks
 # method to list task
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Task
 from .forms import TaskForm
 def task_list(request):
@@ -22,3 +22,22 @@ def task_create(request):
         form = TaskForm()
     return render(request, 'todo/task_form.html',{'form': form})
 
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request,'todo/task_confirm_delete.html',
+                  {'task': task})
+
+def task_update(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'todo/task_form.html',
+                  {'form':form})
